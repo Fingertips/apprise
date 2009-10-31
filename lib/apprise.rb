960da6634447420d008b1076816c7adc6295ac81
Manfred Stienstra <manfred@fngtps.com>
2009-10-31 13:08:38 +0100

    Add binary that shows outdated dependencies.

--- lib/apprise.rb
+++ lib/apprise.rb
@@ -1,1 +1,30 @@
 require 'apprise/plugin'
+
+module Apprise
+  def self.outdated
+    []
+  end
+  
+  def self.run
+    outdated = self.outdated
+    if outdated.empty?
+      puts "There are no outdated dependencies."
+    else
+      puts "Outdated dependencies", ""
+      outdated.each do |name, source_type|
+        puts " * #{name} (#{_humanize_source_type(source_type)})"
+      end
+    end
+  end
+
+  def self._humanize_source_type(source_type)
+    case source_type
+    when 'svn'
+      'Subversion external'
+    when 'git'
+      'Git submodule'
+    else
+      'Gem'
+    end
+  end
+end
--- spec/api/apprise_spec.rb
+++ spec/api/apprise_spec.rb
@@ -1,7 +1,55 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
+module Collector
+  class << self
+    attr_accessor :output
+    
+    def write(string)
+      @output ||= []
+      @output << string
+    end
+    
+    def output
+      @output
+    end
+    
+    def reset!
+      @output.clear if @output
+    end
+  end
+end
+
 describe "Apprise" do
   xit "should return whether or not all dependencies are up-to-date" do
     Apprise.should.not.be.up_to_date
+  end
+  
+  it "should show all outdated dependencies of the application" do
+    Apprise.stubs(:outdated).returns([
+      ['rails', 'git'],
+      ['forestwatcher', 'svn'],
+      ['miso', 'gem']
+    ])
+    
+    collect_stdout do
+      Apprise.run
+    end.should == <<-OUTPUT
+Outdated dependencies
+
+ * rails (Git submodule)
+ * forestwatcher (Subversion external)
+ * miso (Gem)
+OUTPUT
+  end
+  
+  private
+  
+  def collect_stdout
+    Collector.reset!
+    before = $stdout
+    $stdout = Collector
+    yield
+    $stdout = before
+    Collector.output.join
   end
 end
