65056e44b85650873f4239e584ea3dbb8a18da04
Eloy Duran <eloy.de.enige@gmail.com>
2009-10-31 11:15:19 +0100

    Made tests green.

--- spec/api/plugin_spec.rb
+++ spec/api/plugin_spec.rb
@@ -7,6 +7,6 @@ describe "Apprise::Plugin" do
   
   it "should return an instance for each plugin" do
     plugins = Apprise::Plugin.all
-    plugins.map(&:name).should == %w{ authorization-san peiji-san }
+    plugins.map(&:name).should == %w{ git svn }
   end
 end
